a743fe98f920770c9d0f251960635a3a981cffc5
Joel Parker Henderson <joel@joelparkerhenderson.com>
2025-06-26 20:48:39 +0100

    Add llms.txt

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 //! # Web service uptime axum
 //! 
+//! **[documentation](https://docs.rs/web-service-uptime-axum/)**
+//! •
+//! **[source](https://github.com/joelparkerhenderson/web-service-uptime-axum/)**
+//! •
+//! **[llms.txt](https://raw.githubusercontent.com/joelparkerhenderson/web-service-uptime-axum/refs/heads/main/llms.txt)**
+//! •
+//! **[crate](https://crates.io/crates/web-service-uptime-axum)**
+//! •
+//! **[email](mailto:joel@joelparkerhenderson.com)**
+//!
 //! Web service that displays the program uptime by using Axum, Tokio, Rust.
 //! 
 //! This is a very simple web service that we use for testing our systems.
