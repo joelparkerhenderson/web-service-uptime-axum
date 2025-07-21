d837dedc63742f04032cf19288c9fc29b4849b8e
Joel Parker Henderson <joel@joelparkerhenderson.com>
2025-07-21 10:58:33 +0100

    Add /uptime path

--- src/app.rs
+++ src/app.rs
@@ -5,7 +5,7 @@ use axum::routing::get;
 pub fn app() -> axum::Router {
     axum::Router::new()
         .fallback(fallback)
-        .route("/", get(uptime))
+        .route("/uptime", get(uptime))
 }
 
 /// axum handler for any request that fails to match the router routes.
@@ -17,7 +17,7 @@ pub async fn fallback(uri: axum::http::Uri) -> impl axum::response::IntoResponse
 /// Create the constant INSTANT so the program can track its own uptime.
 pub static INSTANT: std::sync::LazyLock<std::time::Instant> = std::sync::LazyLock::new(|| std::time::Instant::now());
 
-/// axum handler for "GET /" which shows the program's uptime duration.
+/// axum handler for "GET /uptime" which shows the program's uptime duration.
 /// This shows how to write a handler that uses a global static lazy value.
 pub async fn uptime() -> String {
     format!("{}", INSTANT.elapsed().as_secs())
@@ -32,9 +32,9 @@ mod tests {
     async fn test() {
         let app: axum::Router = app();
         let server = TestServer::new(app).unwrap();
-        let response_text_0 = server.get("/").await.text();
+        let response_text_0 = server.get("/uptime").await.text();
         std::thread::sleep(std::time::Duration::from_secs(1));
-        let response_text_1 = server.get("/").await.text();
+        let response_text_1 = server.get("/uptime").await.text();
         assert!(response_text_0 < response_text_1, "{} < {}", response_text_0, response_text_1);
     }
 
